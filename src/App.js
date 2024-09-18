5061d914da3341f1ba02907c3021b5f6dfb2a77c
fatihbaydar <fatihbaydar2004@gmail.com>
2024-09-18 17:25:58 +0300

    dark mode

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import FlexAndResponsive from "./components/FlexAndResponsive";
 import HoverAndFocus from "./components/HoverAndFocus";
 import SpacingAndSizing from "./components/SpacingAndSizing";
 import Typography from "./components/Typography";
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-2xl text-center text-purple-500 font-bold mt-2">Tailwind</h1>
+    <div className="dark:bg-gray-700 min-h-screen">
+      <Navbar/>
+      <h1 className="text-2xl text-center text-purple-500 dark:text-white font-bold mt-2">Tailwind</h1>
       {/* <Typography/> */}
       {/* <SpacingAndSizing/> */}
       {/* <HoverAndFocus/> */}
-      <FlexAndResponsive/>
+      {/* <FlexAndResponsive/> */}
     </div>
   );
 }
