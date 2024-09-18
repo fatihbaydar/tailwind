3b46c478eb3894239404001542c6007840ee4639
fatihbaydar <fatihbaydar2004@gmail.com>
2024-09-18 19:16:17 +0300

    index css and end of the study

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import FlexAndResponsive from "./components/FlexAndResponsive";
 import HoverAndFocus from "./components/HoverAndFocus";
 import SpacingAndSizing from "./components/SpacingAndSizing";
 import Typography from "./components/Typography";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
+import ProfileCard from "./components/ProfileCard";
 
 function App() {
   return (
     <div className="dark:bg-gray-700 min-h-screen">
-      <Navbar/>
-      <h1 className="text-2xl text-center text-purple-500 dark:text-white font-bold mt-2">Tailwind</h1>
+      <Navbar />
+      <ProfileCard />
+      <h1 className="mt-2">
+        Tailwind
+      </h1>
       {/* <Typography/> */}
       {/* <SpacingAndSizing/> */}
       {/* <HoverAndFocus/> */}
@@ -18,3 +22,5 @@ function App() {
 }
 
 export default App;
+
+// burada h1 içinde yer alan text-2xl text-center text-purple-500 dark:text-white font-bold kısmı alıp index.css içindeki layer base içinde tanımladık. böylece her yerde tüm h1ler aynı stile sahip olabilecek.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  //* projemize bir plugin dahil etmek istiyorsak config dosyasına eklememiz gerekir
+  // content: ["./node_modules/flowbite/**/*.js"],
+  //? kendi theme ayarlarımızı eklemek için bu kısmı ekliyoruz
   theme: {
-    extend: {},
+    //! eğer tailwind'in default theme ayarları yerine kendimizinkini eklemek istemiyorsak mutlaka extend objesi içinde eklememiz gerekli yoksa tüm theme objesi değişir ve kendi eklediklerimiz dışındakileri kullanamayız.
+
+    extend: {
+      colors: {
+        primary: "#624E88",
+        primary_hover: "#8967B3",
+        dark: "#526D82",
+        darker: "#27374D",
+      },
+    },
   },
-  plugins: [],
-  darkMode: 'selector',
-}
+  //* projemize bir plugin dahil etmek istiyorsak config dosyasına eklememiz gerekir
+  // plugins: [require("flowbite/plugin")],
+  darkMode: "selector",
+};
